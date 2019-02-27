7375d06e8b1ea39b26b9ed912c619f090ea8b824
Tom Uebel <tauebel@gmail.com>
2019-02-26 20:27:16 -0800

    Add a bit of user info per https://github.com/grnhse/omniauth-greenhouse/pull/6/files

--- lib/omniauth/strategies/greenhouse.rb
+++ lib/omniauth/strategies/greenhouse.rb
@@ -7,6 +7,23 @@ module OmniAuth
       def callback_url
         full_host + script_name + callback_path
       end
+
+      info do
+        {
+            :name => "#{raw_info['first_name']} #{raw_info['last_name']}",
+            :email => raw_info['email']
+        }
+      end
+
+      extra do
+        {
+            'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/v1/partner/current_user').parsed
+      end
     end
   end
 end
