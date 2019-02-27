bdaf5106a717624ceee47402011c9a69e5d102a3
Tom Uebel <tauebel@gmail.com>
2019-02-26 20:25:12 -0800

    Add callback URL

--- lib/omniauth/strategies/greenhouse.rb
+++ lib/omniauth/strategies/greenhouse.rb
@@ -3,6 +3,10 @@ module OmniAuth
     class Greenhouse < OmniAuth::Strategies::OAuth2
       option :name, 'greenhouse'
       option :client_options, { :site => "https://api.greenhouse.io" }
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
     end
   end
 end
